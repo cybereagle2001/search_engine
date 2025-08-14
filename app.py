d8fc2049846f37f027bc0fdef8d11c48a116982c
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2025-08-14 23:56:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,6 @@ SEARCH_ENGINES = [
         }
     },
     {
-        'name': 'Bing',
-        'url': 'https://www.bing.com/search',
-        'params': lambda query, page: {
-            'q': query,
-            'first': (page - 1) * 10 + 1
-        }
-    },
     {
         'name': 'DuckDuckGo',
         'url': 'https://html.duckduckgo.com/html/',
